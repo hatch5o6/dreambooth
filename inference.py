a4809fda4ec2428c7b2da0d84cb87f9d04b0950b
hatch5o6 <44300568+hatch5o6@users.noreply.github.com>
2023-09-19 16:15:24 -0600

    now built to generate the holy secretary

--- inference.py
+++ inference.py
@@ -13,11 +13,22 @@ def get_unique_filename(base_filename):
 model_id = "./output_model"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-prompt = "a photo of sks rubber duck at the beach"
+prompts = [
+    "a photo of sks man at the beach",
+    "a photo of sks man dressed as a superhero flying through the city",
+    "a realist painting of sks man sitting in thought under a cherry tree",
+    "a cubist painting of sks man dressed in medieval armor stabbed in the chest",
+    "a pencil drawing of sks man hiking in the mountains",
+    "a photo of sks man hiking in the mountains",
+    "a photo of sks man as a video game character",
+]
+
+# prompt = "a photo of sks rubber duck at the beach"
 #prompt = "PaperCut sks rubber duck at the beach"
 #prompt = "woolitize, sks rubber duck in bright green grass, sun in background, fluffy, daisies"
 
-image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+for prompt in prompts:
+    image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
-output_filename = get_unique_filename("output_model/output.png")
-image.save(output_filename)
+    output_filename = get_unique_filename(f"output_model/{prompt.replace(' ', '_')}.png")
+    image.save(output_filename)
